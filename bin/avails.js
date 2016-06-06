67098dc24693d31a34ac2fd78d9b145b4e8aee9a
HGPA <hector@hectorparra.com>
2016-06-06 11:16:26 -0700

    Remove default setting for Avails command

--- bin/avails.js
+++ bin/avails.js
@@ -7,7 +7,6 @@ var program = require('commander');
 program
   .version(packageJson.version)
   .description(packageJson.description)
-  .command('convert', 'convert Avails between various formats', {
-    isDefault: true
-  })
+  .command('convert', 'convert Avails between various formats')
+  .command('merge', 'merge historical Avails into one')
   .parse(process.argv);
