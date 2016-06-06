32acfc6845d9c3e7f8503eef119a3756ff8e99a0
HGPA <hector@hectorparra.com>
2016-06-06 11:09:57 -0700

    Factor out readAvailsFromStream and writeAvailsToStream
    These will be shared and used by another function

--- bin/avails-convert.js
+++ bin/avails-convert.js
@@ -3,15 +3,8 @@
 var Avails = require('../lib/avails');
 var packageJson = require('../package.json');
 
-// Readers
-var readAvailsFromJSONStream = require('../lib/readAvailsFromJSONStream');
-var readAvailsFromTSVStream = require('../lib/readAvailsFromTSVStream');
-var readAvailsFromXLSXStream = require('../lib/readAvailsFromXLSXStream');
-
-// Writers
-var writeAvailsToJSONStream = require('../lib/writeAvailsToJSONStream');
-var writeAvailsToTSVStream = require('../lib/writeAvailsToTSVStream');
-var writeAvailsToXLSXStream = require('../lib/writeAvailsToXLSXStream');
+const readAvailsFromStream = require('../lib/readAvailsFromStream');
+const writeAvailsToStream = require('../lib/writeAvailsToStream');
 
 var program = require('commander');
 
@@ -47,41 +40,3 @@ readAvailsFromStream(process.stdin, program.input)
     console.error(err.message);
     process.exit(1);
   })
-
-/**
- *
- *
- */
-function readAvailsFromStream (readableStream, input) {
-  switch (input) {
-    case 'tsv':
-      return readAvailsFromTSVStream(readableStream);
-    case 'json':
-      return readAvailsFromJSONStream(readableStream);
-    case 'xlsx':
-      return readAvailsFromXLSXStream(readableStream);
-    default:
-      return new Promise(function (resolve, reject) {
-        reject(new Error('Unsupported input type: ' + input));
-      })
-  }
-}
-
-/**
- *
- *
- */
-function writeAvailsToStream(writableStream, output, avails) {
-  switch (output) {
-    case 'tsv':
-      return writeAvailsToTSVStream(avails, writableStream);
-    case 'json':
-      return writeAvailsToJSONStream(avails, writableStream);
-    case 'xlsx':
-      return writeAvailsToXLSXStream(avails, writableStream);
-    default:
-      return new Promise(function (resolve, reject) {
-        reject(new Error('Unsupported output type: ' + output));
-      })
-  }
-}
